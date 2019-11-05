c7b68bf0b0107c50d7c4eb5338b988f587783b6f
Youssefguba <youssefguba6@gmail.com>
2019-11-05 15:14:35 +0200

    Update MainActivity

--- app/src/main/java/android/techack/colormyview/MainActivity.kt
+++ app/src/main/java/android/techack/colormyview/MainActivity.kt
@@ -1,10 +1,7 @@
 package android.techack.colormyview
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,46 +9,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setListeners()
+
+        // TODO (1) Get (id) of the view
+        // TODO (2) Make the view Clickable
+        // TODO (3) You can make it clickable with the ( When ) Expression
+        //  or you can make it by individual view Like this -> view.onClickListener (It's Just an example :D)
     }
 
-    /**
-     * Attaches listeners to all the views.
-     */
-    private fun setListeners() {
-
-        val clickableViews: List<View> =
-            listOf(box_one_text, box_two_text, box_three_text,
-                box_four_text, box_five_text, constraint_layout)
-
-        for (item in clickableViews) {
-            item.setOnClickListener { makeColored(it) }
-        }
-    }
-
-
-    /**
-     * Sets the background color of a view depending on it's resource id.
-     * This is a way of using one click handler to do similar operations on a group of views.
-     *
-     * Demonstrates three different ways of setting colors.
-     *     * Using a color constant from the Color class.
-     *     * Using an Android color resource
-     *     * Using a custom color defined in color.xml
-     */
-    private fun makeColored(view: View) {
-        when (view.id) {
-
-            // Boxes using Color class colors for background
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
-
-            // Boxes using Android color resources for background
-            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
-            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
-            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
-
-            else -> view.setBackgroundColor(Color.LTGRAY)
-        }
-    }
 }
